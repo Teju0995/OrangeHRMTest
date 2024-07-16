988388d7118bf956408644040f8a06b7161c3371
DELL <DELL@192.168.1.40>
2024-07-16 22:45:13 +0530

    HomePageTitle  TC is created

--- src/test/java/TestLayer/OrangeHRMTest.java
+++ src/test/java/TestLayer/OrangeHRMTest.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class OrangeHRMTest {
@@ -27,6 +28,15 @@ public class OrangeHRMTest {
 		driver.findElement(By.name("username")).sendKeys("Admin");
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		driver.findElement(By.xpath("//button[text()=' Login ']")).click();
+		
+		
+	}
+	
+	@Test(priority=3)
+	public void HomepageTitle()
+	{
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, "OrangeHRM");
 		
 		
 	}
